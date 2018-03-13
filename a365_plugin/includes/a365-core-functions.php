22efa14a5d31ad44fd4ef0ad4ea12ba09fa11a0f
halink0803 <halink0803@gmail.com>
2018-03-14 01:56:23 +0700

    update function from no to doctor name

--- a365_plugin/includes/a365-core-functions.php
+++ a365_plugin/includes/a365-core-functions.php
@@ -8,10 +8,43 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 function formatDate($date) {
 	if (count(split(" ", $date)) > 1 ) {
 		$part = explode("-", split(" ", $date)[0]);
-    	return $part[2].'-'.$part[1].'-'.$part[0];	
+    	return $part[2].'-'.$part[1].'-'.$part[0];
 	}
 	$part = explode("-", $date);
     return $part[2].'-'.$part[1].'-'.$part[0];
+}
+
+function noToNameDiagnose($no) {
+	switch ($no) {
+		case 1:
+			return "Bác sỹ chuyên khoa Nhi"
+			break;
+		case 2:
+			return "Bác sỹ đa khoa"
+			break;
+		case 3:
+			return "Cán bộ tâm lý"
+			break;
+		case 4:
+			return "Bác sỹ chuyên ngành khác"
+			break;
+		case 5:
+			return "Y tá/ Điều dưỡng"
+			break;
+		case 6:
+			return "Giáo viên mầm non"
+			break;
+		case 7:
+			return "Giáo viên Giáo dục đặc biệt"
+			break;
+		case 8:
+			return "Khác"
+			break;
+
+		default:
+			return "Không xác định"
+			break;
+	}
 }
 
 function session_init() {
